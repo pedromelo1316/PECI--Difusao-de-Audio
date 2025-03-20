fd8e68363cfa1591694ab3c042ba8e22f60927ec
Pedro Melo <pedromelo1316@gmail.com>
2025-03-20 16:26:56 +0000

    não sei

--- Main/Manager/app.py
+++ Main/Manager/app.py
@@ -27,17 +27,19 @@ socketio = SocketIO(app)
 BITRATE = "128k"  # Taxa de bits máxima
 SAMPLE_RATE = "48000"  # Taxa de amostragem
 CHUNCK_SIZE = 960
-AUDIO_CHANNELS = "2"  # Mono
+AUDIO_CHANNELS = "1"  # Mono
 
 NUM_CHANNELS = 3  # Número total de canais
 
 # Função para iniciar o processo do ffmpeg para um canal específico
 def start_ffmpeg_process(channel, source, _type):
     # Define o endereço de multicast baseado no número do canal
-    multicast_address = f"rtp://239.255.0.{channel}:12345?pkt_size=2500"
+    multicast_address = f"rtp://239.255.0.{channel}:12345"
     print(f"session_{channel}.sdp")
     print("source: ", source)
     print("type: ", _type)
+    
+    mic = {"card": "1", "device": "0"}  #usar no terminal arecord -l  # Lista dispositivos de captura (microfones) 
     
     # Verifica o tipo de transmissão e configura o comando do ffmpeg apropriado
     if _type == ChannelType.VOICE:
@@ -45,13 +47,15 @@ def start_ffmpeg_process(channel, source, _type):
         cmd = [
             "ffmpeg",
             "-hide_banner", "-loglevel", "error",
-            "-f", "alsa", "-i", source,
-            "-af", "arnndn=model=rnnoise.rnnn",  # Filtro de cancelamento de eco
+            "-f", "alsa",
+            "-i", f"hw:{mic['card']},{mic['device']}",  # Ex: hw:1,0
+            "-af", "afftdn=nt=w:tn=1",  # Filtro para redução de ruído (noise reduction)
             "-acodec", "libopus",
+            "-vn",
             "-b:a", BITRATE,
             "-ar", SAMPLE_RATE,
             "-ac", AUDIO_CHANNELS,
-            "-frame_duration", "120",  # Frames de 40 ms
+            #"-frame_duration", "120",  # Frames de 40 ms
             "-f", "rtp",
             "-sdp_file", f"session_{channel}.sdp",
             f"{multicast_address}"
@@ -76,7 +80,7 @@ def start_ffmpeg_process(channel, source, _type):
             "-vn",
             "-acodec", "libopus",
             "-b:a", BITRATE,
-            "-frame_duration", "120",  # Frames de 40 ms
+            #"-frame_duration", "120",  # Frames de 40 ms
             "-ac", AUDIO_CHANNELS,
             "-f", "rtp",
             "-sdp_file", f"session_{channel}.sdp",
@@ -122,7 +126,7 @@ def start_ffmpeg_process(channel, source, _type):
             "-buffer_size", "1024",  # Aumenta o buffer de saída
             "-max_delay", "200000",  # Limita o atraso máximo
             "-f", "rtp",
-            "-frame_duration", "120",  # Frames de 120 ms
+            #"-frame_duration", "120",  # Frames de 120 ms
             "-sdp_file", f"session_{channel}.sdp",
             "-muxdelay", "0.1",  # Reduz o atraso de muxagem
             "-muxpreload", "0.1",
@@ -131,7 +135,7 @@ def start_ffmpeg_process(channel, source, _type):
     else:
         return None
     # Inicia o subprocesso do ffmpeg
-    process = subprocess.Popen(cmd)
+    process = subprocess.Popen(cmd, preexec_fn=os.setsid)
     return process
 
 # Função para trocar o processo do canal (reinicia se necessário)
@@ -143,7 +147,7 @@ def change_channel_process(channel, source, transmission_type):
     # Se já existir um processo para o canal, termina-o
     if processes[channel]:
         print("Terminating process...")
-        processes[channel].terminate()
+        os.killpg(os.getpgid(processes[channel].pid), signal.SIGTERM)
         processes[channel].wait()
         processes[channel] = None
         
@@ -597,6 +601,12 @@ def shutdown_handler(signum, frame):
     stop_event.set()  # Sinaliza para parar as threads
     thread.join()     # Aguarda a thread de detecção de nós terminar
     # Aqui poderia-se terminar outros subprocessos, se necessário
+    
+    for process in processes.values():
+        if process:
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+            process.wait()
+    
     print("Processes terminated")
     sys.exit(0)  # Encerra o programa de forma limpa
 
