bcaf0f3e7cb46634e2febaa45444f1ed53910a70
pedro miguel miranda melo <pedromelo1316@gmail.com>
2025-04-08 18:20:45 +0100

    node terminar bem

--- Main/Node/main.py
+++ Main/Node/main.py
@@ -40,7 +40,6 @@ def terminate_routine():
         if play_thread is not None:
             print("Stopping play_thread...")
             player_stop_event.set()
-            stop_event.set()
             play_thread.join(timeout=1)
             play_thread = None
     except Exception as e:
@@ -50,7 +49,6 @@ def terminate_routine():
         if ffmpeg is not None:
             print(f"Terminating ffmpeg process {ffmpeg.pid}...")
             try:
-                # Envia sinal de término para o grupo de processos
                 os.killpg(os.getpgid(ffmpeg.pid), signal.SIGTERM)
                 ffmpeg.wait(timeout=5)
             except ProcessLookupError:
@@ -58,7 +56,7 @@ def terminate_routine():
             except subprocess.TimeoutExpired:
                 print(f"Process {ffmpeg.pid} did not terminate in time. Forcing termination...")
                 try:
-                    os.killpg(os.getpgid(ffmpeg.pid), signal.SIGKILL)  # Força o encerramento com SIGKILL
+                    os.killpg(os.getpgid(ffmpeg.pid), signal.SIGKILL)
                     ffmpeg.wait(timeout=5)
                     print(f"Process {ffmpeg.pid} terminated.")
                 except subprocess.TimeoutExpired:
@@ -93,7 +91,7 @@ def shutdown_handler(sig, frame):
     print("Received SIGINT, shutting down...")
     terminate_routine()
     stop_event.set()
-    sys.exit(0)
+    os.exit(0)
 
 def play_audio(n, sdp_file):
     global ffmpeg, player, stream, player_stop_event
@@ -114,7 +112,11 @@ def play_audio(n, sdp_file):
         "-"
     ]
     # Inicia o processo ffmpeg com redirecionamento da saída padrão
-    ffmpeg = subprocess.Popen(ffmpeg_cmd, stdout=subprocess.PIPE)
+    ffmpeg = subprocess.Popen(
+        ffmpeg_cmd,
+        stdout=subprocess.PIPE,
+        preexec_fn=os.setsid   # Cria um novo grupo para o ffmpeg
+    )
     player = pyaudio.PyAudio()
     # Abre um stream de áudio para saída; o formato é PCM com SAMPLE_WIDTH bytes e 2 canais
     stream = player.open(format=player.get_format_from_width(SAMPLE_WIDTH), channels=2, rate=FREQ, output=True)
@@ -150,10 +152,11 @@ def wait_for_info(n, port=8081):
         # Permite reutilização do endereço para evitar erros de "address already in use"
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind(('0.0.0.0', port))
+        server_socket.settimeout(1)
         while not stop_event.is_set():
-            data, addr = server_socket.recvfrom(4096)
-            data = data.decode('utf-8')
             try:
+                data, addr = server_socket.recvfrom(4096)
+                data = data.decode('utf-8')
                 dic = json.loads(data)
                 # Verifica se a mensagem contém a chave que representa o MAC do nó
                 if n.mac in dic.keys():
@@ -177,13 +180,15 @@ def wait_for_info(n, port=8081):
                     new_channel = info["channel"] if "channel" in info.keys() else None
                     volume = info["volume"] if "volume" in info.keys() else None
                     new_HEADER = info["header"] if "header" in info.keys() else None
+                    restart = info["restart"] if "restart" in info.keys() else False
                     
                     # Se volume recebido for diferente do atual, atualiza
                     if volume is not None and n.getVolume() != volume:
                         n.setVolume(float(volume))
                     
-                    # Se o header ou canal mudou, atualiza estação e reinicia o processo do ffmpeg
-                    if (new_HEADER is not None and new_channel is not None) and (HEADER != new_HEADER or new_channel != n.getChannel()):
+                    if restart:
+                        # Se o volume for None, não altera
+                        n.setVolume(None)
                         print("New header or channel received")
                         # Se houver uma thread de reprodução ativa, termina-a
                         terminate_routine()
@@ -199,26 +204,27 @@ def wait_for_info(n, port=8081):
                         # Atualiza o canal do nó
                         n.setChannel(channel)
                         
-                        
+                        print("work")
                     
                             
                         # Se não houver uma thread de reprodução ativa, inicia uma nova
                         if play_thread is None:
+                            print("Starting FFmpeg process...")
                             play_thread = threading.Thread(target=play_audio, args=(n, "session_received.sdp",))
                             play_thread.start()
                             print("FFmpeg process started.")
                         else:
                             # Termina a thread atual do ffmpeg e inicia uma nova, para atualizar o stream
-                            
+                            print("Restarting FFmpeg process...")
                             play_thread = threading.Thread(target=play_audio, args=(n, "session_received.sdp",))
                             play_thread.start()
                             print("FFmpeg process restarted.")
-                    
-                    if new_HEADER is None or new_channel is None:
-                        print("No new header or channel received")
-                        terminate_routine()
-                        player_stop_event.clear()
-                        print("FFmpeg process stopped.")
+                        
+                        if new_HEADER is None or new_channel is None:
+                            print("No new header or channel received")
+                            terminate_routine()
+                            player_stop_event.clear()
+                            print("FFmpeg process stopped.")
                         
                         
                         
@@ -226,6 +232,8 @@ def wait_for_info(n, port=8081):
                     print("Channel:", n.getChannel())
                     print("Volume:", n.getVolume())
 
+            except socket.timeout:
+                continue
             except ValueError as e:
                 print("Error in wait_for_info:", e)
 
@@ -266,38 +274,42 @@ def wait_for_connection(n, port=8080):
 
 def main():
     global play_thread, ffmpeg, HEADER, OP, channel, player, stream
-    # Obtém o nome do host e o endereço MAC do computador atual
-    nome = socket.gethostname()
-    mac = ':'.join([f'{(uuid.getnode() >> i) & 0xff:02x}' for i in reversed(range(0, 48, 8))])
-    # Cria o objeto node_client com as informações do dispositivo
-    n = node_client.node_client(nome, mac)
+    try:
+        # Obtém o nome do host e o endereço MAC do computador atual
+        nome = socket.gethostname()
+        mac = ':'.join([f'{(uuid.getnode() >> i) & 0xff:02x}' for i in reversed(range(0, 48, 8))])
+        # Cria o objeto node_client com as informações do dispositivo
+        n = node_client.node_client(nome, mac)
 
-    
-    # Associa o sinal SIGINT ao shutdown_handler para tratamento de Ctrl+C
-    signal.signal(signal.SIGINT, shutdown_handler)
+        # Associa o sinal SIGINT ao shutdown_handler para tratamento de Ctrl+C
+        signal.signal(signal.SIGINT, shutdown_handler)
 
-    # Cria e inicia a thread responsável por estabelecer a conexão com o gerenciador
-    t_connection = threading.Thread(target=wait_for_connection, args=(n,8080))
-    t_connection.start()    
-    
-    # Cria e inicia a thread que ficará aguardando informações para controle do stream
-    t_info = threading.Thread(target=wait_for_info, args=(n,8081))
-    t_info.start()
+        # Cria e inicia a thread responsável por estabelecer a conexão com o gerenciador
+        t_connection = threading.Thread(target=wait_for_connection, args=(n, 8080))
+        t_connection.start()    
 
-    # Aguarda o término das threads de conexão e recebimento de informações
-    t_connection.join()
-    t_info.join()
-    
-    # Se houver uma thread de reprodução ativa, espera seu término
-    if play_thread is not None:
-        play_thread.join()
+        # Cria e inicia a thread que ficará aguardando informações para controle do stream
+        t_info = threading.Thread(target=wait_for_info, args=(n, 8081))
+        t_info.start()
+
+        # Aguarda o término das threads de conexão e recebimento de informações
+        t_connection.join()
+        t_info.join()
         
-    # Garante o término do processo ffmpeg se estiver ativo
-    if ffmpeg is not None:
-        ffmpeg.terminate()
+        # Se houver uma thread de reprodução ativa, espera seu término
+        if play_thread is not None:
+            play_thread.join()
+            
+        # Garante o término do processo ffmpeg se estiver ativo
+        if ffmpeg is not None:
+            ffmpeg.terminate()
         
-    print("Exiting...")
-    sys.exit(0)
+        print("Exiting...")
+    except Exception as e:
+        print("Unhandled exception in main:", e)
+    finally:
+        terminate_routine()
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
